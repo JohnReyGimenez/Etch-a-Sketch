99546603905fbd7279cdc175bc6353af86203742
JohnReyGimenez <gimenezjr5@gmail.com>
2024-09-16 14:53:59 +0800

    added buttons and a getsize function to select the size for the squarees

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){ 
     createContainer(16);
-    console.log('hi')
 });
 
 
@@ -14,5 +13,20 @@ function createContainer(size) {
 
     for (i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
+    }
+}
+
+function getSize() {
+    let input = prompt("Size of squares: 1 - 100");
+    let message = document.querySelector("#message");
+    if (input == "") {
+        message.innerHTML = "PUT A NUMBER BROO";
+    }
+
+    else if (input < 0 || input > 100) {
+        message.innerHTML = "READ BRO DAMN ITS 1 - 100";
+    }
+    else {
+        message.innerHTML = "";
     }
 }
